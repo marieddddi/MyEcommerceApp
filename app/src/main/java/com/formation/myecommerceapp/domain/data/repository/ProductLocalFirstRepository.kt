c155af6484d9aa76db3f362d5dcb099b3ca405d5
marieddddi <marie.duval@ig2i.centralelille.fr>
2026-06-16 21:23:52 +0200

    GestionQtePanierOk

---
+++ app/src/main/java/com/formation/myecommerceapp/domain/data/repository/ProductLocalFirstRepository.kt
@@ -0,0 +1,58 @@
+package com.formation.myecommerceapp.domain.data.repository
+
+import com.formation.myecommerceapp.domain.data.local.dao.ProductDao
+import com.formation.myecommerceapp.domain.data.local.entity.ProductEntity
+import com.formation.myecommerceapp.domain.data.remote.service.ProductApiService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+
+class ProductLocalFirstRepository(
+    private val productDao: ProductDao,
+    private val productApiService: ProductApiService,
+) : ProductRepository {
+
+    override fun getAll(): Flow<List<ProductEntity>> = flow {
+        // récupération de la bdd
+        val localProducts = productDao.getAll().first()
+
+        if (localProducts.isNotEmpty()) {
+            emit(localProducts)
+        } else {
+            val response = productApiService.getProducts()
+            if (response.isSuccessful) {
+                response.body()?.products?.forEach { dto ->
+                    // produit existe déjà en local ?
+                    val existingProduct = localProducts.find { it.id == dto.id.toInt() }
+
+                    // Si déjà en local, on garde la quantité locale, sinon on se base sur le booléen
+                    val finalQuantity = existingProduct?.quantityInCart
+                        ?: (if (dto.isInCart) 1 else 0)
+
+                    productDao.upsert(
+                        ProductEntity(
+                            id = dto.id.toInt(),
+                            name = dto.name,
+                            description = dto.description ?: "",
+                            isAvailable = dto.isAvailable,
+                            price = dto.price,
+                            category = dto.category,
+                            averageMark = dto.averageMark,
+                            markCount = dto.markCount,
+                            imageDrawable = dto.imageBase64 ?: "",
+                            isFavorite = existingProduct?.isFavorite ?: false,
+                            quantityInCart = finalQuantity
+                        )
+                    )
+                }
+                emit(productDao.getAll().first())
+            }
+        }
+    }
+
+    override suspend fun getById(id: Int): ProductEntity? = productDao.getById(id)
+
+    override suspend fun upsert(product: ProductEntity) = productDao.upsert(product)
+
+    fun getCartProducts(): Flow<List<ProductEntity>> = productDao.getCartProducts()
+}
---
+++ app/src/main/java/com/formation/myecommerceapp/domain/data/repository/ProductRepository.kt
@@ -0,0 +1,10 @@
+package com.formation.myecommerceapp.domain.data.repository
+
+import com.formation.myecommerceapp.domain.data.local.entity.ProductEntity
+import kotlinx.coroutines.flow.Flow
+
+interface ProductRepository {
+    fun getAll(): Flow<List<ProductEntity>>
+    suspend fun getById(id: Int): ProductEntity?
+    suspend fun upsert(product: ProductEntity)
+}
